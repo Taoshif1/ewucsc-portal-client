9356ad23219722d16cfcdda112802acb119f832f
Taoshif1 <taoshif2@gmail.com>
2026-09-25 02:35:19 +0600

    feat(executive): include homework review tools

--- src/pages/dashboards/ExecutiveDashboard.jsx
+++ src/pages/dashboards/ExecutiveDashboard.jsx
@@ -1,6 +1,7 @@
 import MemberDashboard from "./MemberDashboard";
 import CyberOperationsManager from "../../components/admin/CyberOperationsManager";
 import ContentManager from "../../components/admin/ContentManager";
+import SubmissionReviewPanel from "../../components/admin/SubmissionReviewPanel";
 
 const ExecutiveDashboard = () => (
   <>
@@ -8,6 +9,7 @@ const ExecutiveDashboard = () => (
     <div className="mx-auto -mt-14 max-w-7xl px-4 pb-20 md:px-6 lg:px-8">
       <CyberOperationsManager />
       <ContentManager />
+      <SubmissionReviewPanel />
     </div>
   </>
 );
