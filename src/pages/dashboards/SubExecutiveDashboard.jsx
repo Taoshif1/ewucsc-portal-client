0d838febe836a82c7b2052b8c3bdcca78214ed61
Taoshif1 <taoshif2@gmail.com>
2026-09-25 02:09:43 +0600

    feat(sub-exec): connect review workspace

--- src/pages/dashboards/SubExecutiveDashboard.jsx
+++ src/pages/dashboards/SubExecutiveDashboard.jsx
@@ -1,3 +1,13 @@
 import MemberDashboard from "./MemberDashboard";
+import SubmissionReviewPanel from "../../components/admin/SubmissionReviewPanel";
 
-export default MemberDashboard;
+const SubExecutiveDashboard = () => (
+  <>
+    <MemberDashboard />
+    <div className="mx-auto -mt-14 max-w-7xl px-4 pb-20 md:px-6 lg:px-8">
+      <SubmissionReviewPanel />
+    </div>
+  </>
+);
+
+export default SubExecutiveDashboard;
