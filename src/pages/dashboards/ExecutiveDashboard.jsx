1071bd8679d90f47db02198830a521c0684ce524
Taoshif1 <taoshif2@gmail.com>
2026-09-25 02:13:33 +0600

    feat(content): finish admin publishing integration

--- src/pages/dashboards/ExecutiveDashboard.jsx
+++ src/pages/dashboards/ExecutiveDashboard.jsx
@@ -1,11 +1,13 @@
 import MemberDashboard from "./MemberDashboard";
 import CyberOperationsManager from "../../components/admin/CyberOperationsManager";
+import ContentManager from "../../components/admin/ContentManager";
 
 const ExecutiveDashboard = () => (
   <>
     <MemberDashboard />
     <div className="mx-auto -mt-14 max-w-7xl px-4 pb-20 md:px-6 lg:px-8">
       <CyberOperationsManager />
+      <ContentManager />
     </div>
   </>
 );
