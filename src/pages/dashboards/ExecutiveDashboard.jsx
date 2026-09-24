8dec380e9b1b3b1de1eeb4490f4eb55858ba7f95
Taoshif1 <taoshif2@gmail.com>
2026-09-25 03:02:34 +0600

    feat(contact): expose inbox to club staff

--- src/pages/dashboards/ExecutiveDashboard.jsx
+++ src/pages/dashboards/ExecutiveDashboard.jsx
@@ -2,6 +2,7 @@ import MemberDashboard from "./MemberDashboard";
 import CyberOperationsManager from "../../components/admin/CyberOperationsManager";
 import ContentManager from "../../components/admin/ContentManager";
 import SubmissionReviewPanel from "../../components/admin/SubmissionReviewPanel";
+import ContactInbox from "../../components/admin/ContactInbox";
 
 const ExecutiveDashboard = () => (
   <>
