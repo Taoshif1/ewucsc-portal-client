f0b007e17f2253ad54ad43063db895fb7ea4a084
Taoshif1 <taoshif2@gmail.com>
2026-09-25 01:58:46 +0600

    feat(portal): build homepage-style member dashboard

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,15 +1,15 @@
 import { Outlet } from "react-router";
-import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const DashboardLayout = () => {
   return (
-    <div className="min-h-screen bg-base-200">
-      <div className="mx-auto flex max-w-[1600px]">
-        <Sidebar />
-        <main className="flex-1">
-          <Outlet />
-        </main>
-      </div>
+    <div className="min-h-screen flex flex-col bg-base-100">
+      <Navbar />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 };
