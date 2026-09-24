02a139ff2cd0b27f6058221ec07db523386e337a
Taoshif1 <taoshif2@gmail.com>
2026-09-25 02:08:39 +0600

    feat(executive): add challenge and homework management

--- src/pages/dashboards/ExecutiveDashboard.jsx
+++ src/pages/dashboards/ExecutiveDashboard.jsx
@@ -1,3 +1,13 @@
 import MemberDashboard from "./MemberDashboard";
+import CyberOperationsManager from "../../components/admin/CyberOperationsManager";
 
-export default MemberDashboard;
+const ExecutiveDashboard = () => (
+  <>
+    <MemberDashboard />
+    <div className="mx-auto -mt-14 max-w-7xl px-4 pb-20 md:px-6 lg:px-8">
+      <CyberOperationsManager />
+    </div>
+  </>
+);
+
+export default ExecutiveDashboard;
