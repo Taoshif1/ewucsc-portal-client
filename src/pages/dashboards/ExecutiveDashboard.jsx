a692cebeb34a2ec9a52313aca84ae5ac7693df6d
Taoshif1 <taoshif2@gmail.com>
2026-09-25 14:39:19 +0600

    refactor(dashboard): make executive dashboard an operations landing

--- src/pages/dashboards/ExecutiveDashboard.jsx
+++ src/pages/dashboards/ExecutiveDashboard.jsx
@@ -1,18 +1,7 @@
-import MemberDashboard from "./MemberDashboard";
-import CyberOperationsManager from "../../components/admin/CyberOperationsManager";
-import ContentManager from "../../components/admin/ContentManager";
-import SubmissionReviewPanel from "../../components/admin/SubmissionReviewPanel";
-import ContactInbox from "../../components/admin/ContactInbox";
+import StaffDashboardHome from "../../components/dashboard/StaffDashboardHome";
 
 const ExecutiveDashboard = () => (
-  <>
-    <MemberDashboard />
-    <div className="mx-auto -mt-14 max-w-7xl px-4 pb-20 md:px-6 lg:px-8">
-      <CyberOperationsManager />
-      <ContentManager />
-      <SubmissionReviewPanel />
-    </div>
-  </>
+  <StaffDashboardHome role="executive" />
 );
 
 export default ExecutiveDashboard;
