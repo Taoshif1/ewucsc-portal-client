82f30aedf2dc3f455b931812e44538f94bab7d9d
Taoshif1 <taoshif2@gmail.com>
2026-09-25 19:33:07 +0600

    feat(credits): keep build credit on error pages

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,13 @@
 import NotFound from "../components/common/NotFound";
+import SiteCreditBar from "../components/SiteCreditBar";
 
-const ErrorPage = () => {
-  return <NotFound />;
-};
+const ErrorPage = () => (
+  <div className="min-h-screen flex flex-col">
+    <div className="flex-1">
+      <NotFound />
+    </div>
+    <SiteCreditBar />
+  </div>
+);
 
 export default ErrorPage;
