f687e703a211f9db9c67abb11f6895a645fd2ad7
Taoshif1 <taoshif2@gmail.com>
2026-09-25 15:47:03 +0600

    refactor(home): keep public homepage club-focused

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,7 @@
 import HeroSection from "../components/home/HeroSection";
 import ActivitiesSection from "../components/home/ActivitiesSection";
-import LearningTracksSection from "../components/home/LearningTracksSection";
 import WhyJoinSection from "../components/home/WhyJoinSection";
 import CTASection from "../components/home/CTASection";
-import CyberBadgeStrip from "../components/home/CyberBadgeStrip";
 import FAQSection from "../components/home/FAQSection";
 import MouseGlow from "../components/home/MouseGlow";
 import ScrollProgress from "../components/home/ScrollProgress";
@@ -18,9 +16,7 @@ const Home = () => {
 
       <div className="relative z-10 space-y-24 lg:space-y-32">
         <HeroSection />
-        <CyberBadgeStrip />
         <ActivitiesSection />
-        <LearningTracksSection />
         <WhyJoinSection />
         <FAQSection />
         <CTASection />
